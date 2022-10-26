4c8368a41abb64328c23c61dcae8627f3186cef2
MingYu <MingYu@petalmail.com>
2022-10-26 16:29:44 +0800

    首次

---
+++ src/li/one.java
@@ -0,0 +1,11 @@
+package li;
+
+/**
+ * @Author LiMingYu
+ * @Create 2022-10-26 16:28
+ */
+public class one {
+    public static void main(String[] args) {
+        System.out.println("这是首次");
+    }
+}
