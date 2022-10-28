94ec11dc6420b88d1e3628a71073b68c65820288
Li-YanyuPingsheng <94470095+Li-YanyuPingsheng@users.noreply.github.com>
2022-10-28 12:59:12 +0800

    Add files via upload

---
+++ TestFuture.java
@@ -0,0 +1,15 @@
+package com.li;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Author LiMingYu
+ * @Create 2022-10-25 22:36
+ */
+public class TestFuture {
+    static ExecutorService pool = Executors.newFixedThreadPool(2);
+
+    public static void main(String[] args) {
+    }
+}
